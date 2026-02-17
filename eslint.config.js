95f314604ac7624989abc0fd6ea0dd2169098cc4
Marcus-NuGraphix <marcus.nugraphix@gmail.com>
2026-02-17 13:49:30 +0200

    chore: fix eslint version, ignores, and FieldError lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,24 @@
-//  @ts-check
-
+// @ts-check
 import { tanstackConfig } from '@tanstack/eslint-config'
 
-export default [...tanstackConfig]
+export default [
+  // Ignore generated / tooling folders and config files
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/.output/**',
+      '**/.vinxi/**',
+      '**/dist/**',
+      '**/build/**',
+      '**/coverage/**',
+      '**/.agents/**',
+
+      // Tooling configs (do not lint with TS project rules)
+      'eslint.config.js',
+      'prettier.config.js',
+      'vite.config.ts',
+    ],
+  },
+
+  ...tanstackConfig,
+]
